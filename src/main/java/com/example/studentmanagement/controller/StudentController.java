3910caff9613c07f5e65abd5174a8d70ea17c8f1
kishorchisquare <kvinod@chisquarelabs.com>
2026-01-27 12:30:17 +0530

    removed optional

--- src/main/java/com/example/studentmanagement/controller/StudentController.java
+++ src/main/java/com/example/studentmanagement/controller/StudentController.java
@@ -5,7 +5,6 @@ import com.example.studentmanagement.service.StudentService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/students")
